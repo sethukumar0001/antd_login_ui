3c741d23d26574d0927c06ffc27b747a64c99f0a
sethukumar0001 <boylooksrock@gmail.com>
2019-10-24 22:56:30 +0530

    antd ui

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+import { Form, Icon, Input, Button, Checkbox,Card } from 'antd';
+
+class App extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+        <div style={{ background: '#ECECEC', padding: '30px' }}>
+    <Card title="Login" bordered={false} style={{ width: 300 }}>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!123</a>
+        </Form.Item>
+      </Form>
+      </Card>
+      </div>
+    );
+  }
+}
+
+export default Form.create()(App);
+
+
+
+
